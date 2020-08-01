e7b8327bd455aec957509700376b19da41c37780
Michael Thomas <mthomas@Michaels-MacBook-Pro.local>
2020-07-31 21:51:06 -0700

    partial working google api.

--- src/screens/PaymentScreens/MakePaymentScreen.js
+++ src/screens/PaymentScreens/MakePaymentScreen.js
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import {
   AppBackGroundColor,
   AppHeaderTextColor,
@@ -6,108 +6,152 @@ import {
   AppActionButtonColor,
 } from '../../AppSettings';
 import { Text, View, StyleSheet } from 'react-native';
+import { utils } from '@react-native-firebase/app';
+import vision from '@react-native-firebase/ml-vision';
 import ImagePicker from 'react-native-image-picker';
 import { imagePickerOptions } from '../../utils';
 import { useUpload } from '../../hooks';
-import { Button, BackButton, HelpButton } from '../../components';
-import { goBack } from '../../constants';
+import { Button, BackButton, HelpButton, Toast } from '../../components';
+import { goBack, onScreen } from '../../constants';
+import { ActivityIndicator } from 'react-native-paper';
 
 const MakePaymentScreen = ({ navigation }) => {
-  const [{ downloadURL }, monitorUpload] = useUpload();
-  const [ocrresponse, setOCRResponse] = useState('');
-  const pickImage = () => {
-    ImagePicker.launchImageLibrary(imagePickerOptions, imagePickerResponse => {
-      const { didCancel, error } = imagePickerResponse;
+
+  const [validPerson, setValidPerson] = useState(false);
+  const [validAmount, setValidAmount] = useState(false);
+  const [validCode, setValidCode] = useState(false);
+  const [userCode, setUserCode] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const [showManualButton, setShowManualButton] = useState(false);
+
+  const processDocument = async (localPath) => {
+    const processed = await vision().cloudDocumentTextRecognizerProcessImage(localPath);
+
+    processed.blocks.forEach(block => {
+      if (block.text.includes('William Furey')) {
+        if (!validPerson) {
+          setValidPerson(true)
+        }
+      } 
+
+      if (block.text.includes('$498.00')) {
+        if (!validAmount)
+          setValidAmount(true)
+      } 
+
+      if (block.text.includes(userCode)) {
+        setValidCode(true);
+      } 
+    });
+  }
+
+  const randomFixedInteger = (length) => {
+    let userCode = Math.floor(Math.pow(10, length - 1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1));
+    return userCode;
+  }
+
+  const imagePickerOptions = {
+    quality: 0.7,
+    allowsEditing: true,
+    mediaType: 'photo',
+    noData: true,
+    storageOptions: {
+      skipBackup: true,
+      waituntilSaved: false,
+      path: 'images',
+      cameraRoll: true,
+    },
+  };
+
+  const chooseImage = () => {
+    ImagePicker.launchImageLibrary(imagePickerOptions, response => {
+      const { didCancel, error } = response;
       if (didCancel) {
         console.log('Canceled');
       } else if (error) {
         console.log('An error occurred: ', error);
       } else {
-        monitorUpload(imagePickerResponse);
+        let source = response.uri;
+        submitToGoogle(source);
       }
     });
-  };
+  }
 
-  submitToGoogle = async () => {
-    try {
-      let body = JSON.stringify({
-        requests: [
-          {
-            features: [
-              { type: "TEXT_DETECTION", maxResults: 5 },
-            ],
-            image: {
-              source: {
-                imageUri: downloadURL
-              }
-            },
-          }
-        ]
-      });
-      let response = await fetch(
-        "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyATYprb4eLWXejFSBdBzSZwgwcz1n5FMaA",
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          },
-          method: "POST",
-          body: body
+  const submitToGoogle = (filePath) => {
+    setToastMessage('Validating Payment');
+    processDocument(filePath)
+      .then(() => setToastMessage('Completed Payment Validation'))
+      .catch(e => console.log(e))
+      .done(() => {
+        console.log('P: ', validPerson, 'A: ', validAmount, 'C: ', validCode);
+        if (validPerson && validAmount && validCode) {
+          setToastMessage('Your Payment Succesfully Validated')
+          goBack(navigation)();
+        } else {
+          setToastMessage('Oops! Unable to validate your payment')
+          setShowManualButton(true);
         }
-      );
-      let responseJson = await response.json();
-      //setOCRResponse(responseJson);
-      console.log(JSON.stringify(responseJson.responses));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+        setValidAmount(false);
+        setValidCode(false);
+        setValidPerson(false);
+      });
+  }
 
+  useEffect(() => {
+    setLoading(true);
+    let userCode = randomFixedInteger(6);
+    setUserCode(userCode);
+    setLoading(false);
+  }, []);
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerView}>
-        <BackButton style={styles.backButton} goBack={goBack(navigation)} />
-        <HelpButton style={styles.helpButton} />
+  return loading ?
+    <ActivityIndicator size='large' />
+    : (
+      <View style={styles.container}>
+        <View style={styles.headerView}>
+          <BackButton style={styles.backButton} goBack={goBack(navigation)} />
+          <HelpButton style={styles.helpButton} />
+        </View>
+        <View style={[styles.elementsContainer]}>
+          <View>
+            <Text style={styles.headerStyle}>
+              Making Payment With {'PaymentType'} &#38; Verifying
+          </Text>
+          </View>
+          <View style={styles.instructionView}>
+            <Text style={styles.instructionText}>
+              1. Make payment for {'$100.00'} with Venmo sending it to @william-furey-2
+          </Text>
+            <Text style={styles.instructionText}>
+              2. In the message please put this code: {userCode},
+            also choose private. Click Pay.
+          </Text>
+            <Text style={styles.instructionText}>
+              3. Find the transcation that you just completed, and select it.
+          </Text>
+            <Text style={styles.instructionText}>
+              4. Your transcation should say the following:
+              "You paid William Furey -$100.00 in message your unique code {userCode}".
+              Take a screenshot and save it as an photo.
+          </Text>
+            <Text style={styles.instructionText}>
+              5. Click on Verify Payment and upload the screen shot you just took
+          </Text>
+            <Button style={styles.buttonText} onPress={chooseImage}>Verify Payment</Button>
+            {showManualButton && <Button style={styles.buttonText} onPress={''}>Manually Verify Payment</Button>}
+          </View>
+          <View style={styles.instructionView}>
+            <Text style={styles.instructionText}>
+              * If payment verfication fails you can retry again or click on manually
+              verify payment.
+          </Text>
+          </View>
+        </View>
+        <Toast message={toastMessage} onDismiss={() => setToastMessage('')} />
+
       </View>
-      <View style={[styles.elementsContainer]}>
-        <View>
-          <Text style={styles.headerStyle}>
-            Making Payment With {'PaymentType'} &#38; Verifying
-          </Text>
-        </View>
-        <View style={styles.instructionView}>
-          <Text style={styles.instructionText}>1. Choose payment amount.</Text>
-          <Text style={styles.instructionText}>
-            1. Make payment with Venmo sending it to @william-furey-2
-          </Text>
-          <Text style={styles.instructionText}>
-            2. In the message please put this code {'random code for user'},
-            also choose private.
-          </Text>
-          <Text style={styles.instructionText}>
-            3. Find the transcation that you just completed, and select it.
-          </Text>
-          <Text style={styles.instructionText}>
-            4. You should now see "You Paid William Fury" with dollar amount and
-            special code. Now take a screen shot and save it'
-          </Text>
-          <Text style={styles.instructionText}>
-            5. Click on Verify Payment and upload the screen shot you just took
-          </Text>
-          <Button style={styles.buttonText} onPress={pickImage}>Verify Payment</Button>
-          <Button style={styles.buttonText} onPress={submitToGoogle}>Manually Verify Payment</Button>
-        </View>
-        <View style={styles.instructionView}>
-          <Text style={styles.instructionText}>
-            * If successful you will see a check mark next to payment you. If
-            payment verfication fails you can retry again or click on manually
-            verify payment.
-          </Text>
-        </View>
-      </View>
-    </View>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
@@ -151,6 +195,7 @@ const styles = StyleSheet.create({
   },
   instructionText: {
     color: AppTextColor,
+    paddingBottom: 3,
   },
   instructionView: {
     flex: 1,
